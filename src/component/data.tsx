f9cb7fb7a2e257581e5353f51fe45b1f50de32d1
Jessie Nguyen <t-jenguyen@microsoft.com>
2021-06-14 12:45:48 -0400

    initialized project and basic frontend

---
+++ src/component/data.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Data = () => {
+    return (
+        <div>
+            <h3>Fetch API and display data</h3>
+            <input type="text" name="api_choice" id="api_choice" />
+            <button>Go!</button>
+        </div>
+    )
+}
+
+export default Data
