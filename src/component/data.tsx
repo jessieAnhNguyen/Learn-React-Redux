72fc9cb653a0c3aa2548e4a5d0db8e8a91d9b5c2
Jessie Nguyen <t-jenguyen@microsoft.com>
2021-06-16 11:20:14 -0400

    implemented display data from response and used semantic ui for formatting

--- src/component/data.tsx
+++ src/component/data.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { changeUniName, getResponse } from '../actions/dataActions';
 import { DataState } from '../reducers/dataReducer';
+import { Button, Header, Input, Table } from 'semantic-ui-react';
 
 function Data() {
 
@@ -20,19 +21,66 @@ function Data() {
     const getAPI = () => {
         dispatch(getResponse(uniName));
     };
+    
+    return (
+        <div>
+            <Header as='h3'>Fetch universities API and display data</Header>
+            <Input icon='search' placeholder='Search...' onChange={onChangeUniName} />
+            <p>Current university name is {uniName}</p>
+            {/* <div>
+                <button onClick={getAPI}>Go!</button>
+            </div> */}
+            <Button primary onClick={getAPI}>Go!</Button>
+            {/* <ol>
+                {
+                response &&
+                response.map((item: any) => {
+                    return (
+                        <li>
+                            <ul>
+                                <li key={item.name}>{item.name}</li>
+                                <li key={item.web_pages}>{item.web_pages}</li>
+                                <li key={item.country}>{item['country']}</li>
+                            </ul>
+                        </li>
+                    
+                    );
+                })
+                }
+            </ol> */}
 
-    // const listOfNames = (listHere:[]):string => {
-    //     let toReturn:string = "";
+            {response && 
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>University Name</Table.HeaderCell>
+                        <Table.HeaderCell>Website</Table.HeaderCell>
+                        <Table.HeaderCell>Domain</Table.HeaderCell>
+                        <Table.HeaderCell>Country</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
 
-    //     listHere.map((eachItem:any)=>{
-    //        toReturn = toReturn + eachItem.name;
-    //     })
-
-    //     console.log("returning");
-    //     console.log(toReturn);
-
-    //     return listHere[0];
-    // }
+                <Table.Body>
+                    {
+                    response.map((item: any) => {
+                        return (
+                            <Table.Row>
+                                <Table.Cell key={item.name}>{item.name}</Table.Cell>
+                                <Table.Cell key={item.web_pages}>{item.web_pages}</Table.Cell>
+                                <Table.Cell key={item.domains}>{item.domains}</Table.Cell>
+                                <Table.Cell key={item.country}>{item['country']}</Table.Cell>
+                            </Table.Row>
+                        
+                        );
+                    })
+                    }
+      
+                </Table.Body>
+            </Table>
+            }
+            
+        </div>
+    )
 
     const displayResponse = () => {
         console.log("hello")
@@ -73,23 +121,6 @@ function Data() {
         //     )
         // }
     }
-
-    return (
-        <div>
-            <h3>Fetch API and display data</h3>
-            <input 
-                type="text" 
-                onChange={onChangeUniName}
-                name="uniName" 
-                id="uniName"
-            />
-            <span>Current university name is {uniName}</span>
-            <div>
-                <button onClick={getAPI}>Go!</button>
-            </div>
-            <textarea name="" id="" value={displayResponse()} cols={30} rows={10}></textarea>
-        </div>
-    )
 }
 
 export default Data
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store/dataStore';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
